3070eb8eb02df72f8d6355874ee889a5095c1626
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-09-01 18:44:10 -0400

    linked home and about page to app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Sidebar from "./Sidebar";
 import Home from "./Pages/Home";
-import About from "./Pages/About"
+import About from "./Pages/About";
 
 const App = () => {
   return (
