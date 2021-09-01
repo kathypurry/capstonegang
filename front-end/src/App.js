434291969cbd596f1404fb5db9261546b4af57c3
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-09-01 18:31:06 -0400

    initialized components and pages folders

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,25 @@
-import Sidebar from './Sidebar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Sidebar from "./Sidebar";
+import Home from "./Pages/Home";
+import About from "./Pages/About"
 
 const App = () => {
   return (
-    <div className='App'>
-      <Sidebar />
-
+    <div className="App">
+      <Router>
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
 export default App;
