020b860d1c2782effbbc3d4576addcd20b774d95
Jon Narine <jonnicwolf@gmail.com>
2021-09-03 16:09:27 -0400

    timer component complete

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import Timer from "../components/Timer.js"
+import React, { useState } from 'react';
+import Timer from "../components/Timer.js";
 
 const Home = () => {
+    const [time, setTime] = useState(0);
+    const timer = (number_ofSeconds) => {
+        let tMinus = number_ofSeconds
+        const countdown = () => {
+            setTime(tMinus--);
+            if (tMinus > 0) {
+                clearInterval(timer);
+            };
+            return tMinus;
+        };
+        return setInterval(countdown, 1000);
+    };
     return (
         <div>
-            <Timer />
             <h2>Home</h2>
+            <Timer timer={timer} time={time} />
         </div>
     )
-}
+};
 
 export default Home;
--- front-end/src/components/Timer.js
+++ front-end/src/components/Timer.js
@@ -1,24 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const Timer = () => {
-    const timer = (number_ofSeconds) => {
-        let tMinus = number_ofSeconds
-        const countdown = () => {
-            tMinus--;
-            if (tMinus > 0) {
-                clearInterval(timer)
-            }
-            return tMinus
-        }
-        const timer = setInterval(countdown, 1000)
-        
-    };    
+const Timer = ({ timer, time }) => {
     
     return (
         <div id="timer">
-            {timer(30)}
+            {time}
+            <button onClick={()=>timer(30)}>Start</button>
         </div>
     )
-}
+};
 
-export default Timer
+export default Timer;
