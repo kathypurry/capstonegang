a35013aaae603c0c8b54c130c778d3aa12838742
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-09-03 13:27:09 -0400

    small change on About Page - personal test

--- front-end/src/Pages/About.js
+++ front-end/src/Pages/About.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 import MotionF from './MotionF';
+
+const Container = styled.div`
+    
+`
+
+const AboutUs = styled.div`
+    color: var(--foam);
+`
+
+const Details = styled.div`
+
+`
 
 const AboutComponent = () => {
     return (
-        <div>
-            <h2>About</h2>
-        </div>
-    )   ;  
+        <Container>
+            <AboutUs>
+                <h2>About Us</h2>
+            </AboutUs>
+
+            <Details>
+                <p>In the world of typing <br /> we the developers want to be able to produce product in an accurate and time fashion, <br /> but how do we get there?</p>
+            </Details>
+
+        </Container>
+    );  
 };
 
 const About = MotionF(AboutComponent);
