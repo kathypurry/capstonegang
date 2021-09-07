5c1c9a13ae1bcf246b015c93165c9a759a6db578
Kathy Gee <mzchick@gmail.com>
2021-09-07 12:27:23 -0400

    seed data

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -19,6 +19,7 @@ INSERT INTO code_snippets (
 (1, '`Tommy is ${age} years old.` const currency = "$"'),
 (1, 'console.log(`Billy is ${6+8} years old.`) epic = true'),
 (1, 'const multiply = (a,b) => a * b;'),
+(1, 'console.log(`this is ${string} interpolation`)'),
 (2, 'const checkWeight = weight => {
     \nconsole.log(`Baggage weight: ${weight} kilograms.`)};
 '),
