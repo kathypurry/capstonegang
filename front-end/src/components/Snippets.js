13ef49ebe56f1fc70e2709aa7d2a29d02eb7180d
Grace Bong <gracebong.park@gmail.com>
2021-09-09 02:11:10 -0400

    attempt to sort snippets

--- front-end/src/components/Snippets.js
+++ front-end/src/components/Snippets.js
@@ -2,51 +2,51 @@ import { useState, useEffect } from "react";
 import { apiURL } from "../util/apiURL";
 import axios from "axios";
 
-
 import DisplaySnippets from "./DisplaySnippets";
-
 
 export default function Snippets() {
   const API = apiURL();
 
-  //const [snippet, setSnippet] = useState([]);
+  const [snippet, setSnippet] = useState([]);
   const [difficulty, setDifficulty] = useState([])
   const [currentLevel, setCurrentLevel] = useState([]);
 
   useEffect(() => {
     axios.get(`${API}/code`)
-      .then((res) => {
-      const filtered = filterSnippets(res.data) 
-      setCurrentLevel(filterSnippets(res.data));
-      console.log(res.data)
-    });
-  });
-
+      .then(
+        (res) => setSnippet(res.data),
+        (error) => console.log('get', error)
+      )
+      .catch((c) => console.warn('catch', c));
+  }, []);
 
   const handleDifficultyChange = (e) => {
-    setDifficulty({ [e.target.id]: Number(e.target.value) });
+    setDifficulty({ [e.target.id]: e.target.value });
+    const filteredSnippet = snippet.filter((snip) => snip.difficulty === difficulty.difficulty)
+    setCurrentLevel([ filteredSnippet ])
+    console.log(currentLevel)
   };
-
-  const filterSnippets = (data) => {
-    return data.filter((snippet) => (Number(snippet.difficulty)) === difficulty)
-  }
-
+  
   return (
     <div>
       <h1>Snippets</h1>
 
       <label htmlFor='difficulty'>Pick your difficulty</label>
-      <select id='difficulty' onClick={handleDifficultyChange}>
-        <option value='1'>1</option>
+      <select id='difficulty' onChange={handleDifficultyChange}>
+        <option value='1' defaultChecked>1</option>
         <option value='2'>2</option>
         <option value='3'>3</option>
       </select>
 
-
-
-      <h4>{}</h4>
-      {snippet.map((snip) => snip.snippet)}
-
+      <ul>
+      {/* {currentLevel.map(snip => {
+        return (
+          <li key={snip.id}>
+            <DisplaySnippets snippet={snip}/>
+          </li>
+        )
+      })} */}
+      </ul>
     </div>
   );
-}
+};
