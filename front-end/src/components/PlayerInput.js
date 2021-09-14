14313ec9d7947fdffd38691964933f3c5f23fabf
Jon Narine <jonnicwolf@gmail.com>
2021-09-14 14:03:19 -0400

    import path corrections

---
+++ front-end/src/components/PlayerInput.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+
+const PlayerInput = () => {
+    const [input, setInput] = useState("");
+
+    const snippetDiffer = (e) => {
+        let playerInput        
+
+    }
+
+
+    return (
+        <div id="player-input">
+            <input type="text" name="" id="" />
+        </div>
+    )
+}
+
+export default PlayerInput
