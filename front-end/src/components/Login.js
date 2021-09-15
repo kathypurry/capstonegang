cd8fb493b0074fe185febf757cf940894e1a7600
Candice <Candice.Agard@gmail.com>
2021-09-15 13:44:01 -0400

    Auth basic styling

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { firebase } from '../Services/firebase';
 import styled from 'styled-components';
 
+const LoginContainer = styled.div`
+    text-align: center;
+    
+   margin: 30vh;
+`
+
 const InstructHeader = styled.h2`
     margin-bottom: 10px;
+
+    color: var(--white);
 `
 
 const LoginButton = styled.button`
     border: none;
     border-radius: 20px;
+   
 
     width: 100%;
     padding: 15px 0;
@@ -16,6 +25,13 @@ const LoginButton = styled.button`
     font-size: 16px;
     background: var(--grey);
     cursor: pointer;
+
+    &:hover {
+        border-radius: 10px;
+
+        background: var(--neongreen);
+        color: var(--grey);
+    }
 `
 
 const Login = () => {
@@ -32,12 +48,12 @@ const Login = () => {
     }
 
     return (
-        <div>
+        <LoginContainer>
             <InstructHeader>Sign In</InstructHeader>
             <LoginButton onClick={SignInWithFirebase}>
-                <span> Sign In with Google </span>
+                <span> Login In with Google </span>
             </LoginButton>
-        </div>
+        </LoginContainer>
     )
 };
 
