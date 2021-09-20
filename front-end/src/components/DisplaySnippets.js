f2006d39dcbc02294e0772b2aa10bf66e3f0ec7f
Jon Narine <jonnicwolf@gmail.com>
2021-09-20 15:29:30 -0400

    textarea box background invisible with red border for development

--- front-end/src/components/DisplaySnippets.js
+++ front-end/src/components/DisplaySnippets.js
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 
 function DisplaySnippets({snippet}) {
     const [input, setInput] = useState('');
-    const [workingSnippet, setWorkingSnippet] = useState('');
-    
+    const [workingSnippet, setWorkingSnippet] = useState([]);
+
     const Correct = styled.div`
         color: (--neongreen)
     `
@@ -16,30 +16,33 @@ function DisplaySnippets({snippet}) {
     const allSnippets = snippet.map((oneSnippet) => oneSnippet.snippet);
     const superString = allSnippets.join('').split('');
     
-    useEffect(() => {        
+    useEffect(() => {
         console.log(superString, 'superString here!')
         setWorkingSnippet(superString)
         for (
             let i = 0, len = input.length;
             i < len;
-            i++)
-        {
-            input[i] === workingSnippet[i]
-                ? <Correct></Correct>
-                : null
+            i++) {
+            input[i] === superString[i]
+                ? <Correct>{input}</Correct>
+                : <Incorrect>{input}</Incorrect>
         }
-    })
-    const handle_userInput = (e) => {        
-        setInput(e.target.value)        
-    }
+    });
+
+    const handle_userInput = (e) => {
+        setInput(e.target.value)
+    };
+
     return (
         <div>
             <br/>
             <br/>
             <br />
-            {}
+            
             <h3 style={{ whiteSpace: 'pre-wrap' }}>{allSnippets}</h3>
+
             <textarea
+                style={{ background: 'none', borderColor: 'red', width: '45rem', height: '6rem'}}
                 name=""
                 id=""
                 cols="30"
