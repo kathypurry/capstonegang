1d46855861432374e25cc48b44dd3cf5a69ec0e3
Grace Bong <gracebong.park@gmail.com>
2021-09-23 23:14:46 -0400

    commented out cursor component

--- front-end/src/components/BlinkingCursor.js
+++ front-end/src/components/BlinkingCursor.js
@@ -1,23 +1,23 @@
-function BlinkingCursor({ currentSnippet }) {
-    //https://medium.com/@bharatgupta_2334/how-to-create-blinking-cursor-effect-using-javascript-fad099e8bcb9
-    let cursor = true;
-    let speed = 350;
+// function BlinkingCursor({ currentSnippet }) {
+//     //https://medium.com/@bharatgupta_2334/how-to-create-blinking-cursor-effect-using-javascript-fad099e8bcb9
+//     let cursor = true;
+//     let speed = 350;
     
-    setInterval(() => {
-        if(cursor) {
-            document.getElementsByClassName('cursor')[0].style.opacity = 0;
-            cursor = false;
-        } else {
-            document.getElementsByClassName('cursor')[0].style.opacity = 1;
-            cursor = true;
-        }
-    }, speed);
+//     // setInterval(() => {
+//     //     if(cursor) {
+//     //         document.getElementsByClassName('cursor')[0].style.opacity = 0;
+//     //         cursor = false;
+//     //     } else {
+//     //         document.getElementsByClassName('cursor')[0].style.opacity = 1;
+//     //         cursor = true;
+//     //     }
+//     // }, speed);
 
-    return (
-        <div>
-            <h2 className='cursor' style={{color: 'white'}}>|</h2>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <h2 className='cursor' style={{color: 'white'}}>|</h2>
+//         </div>
+//     );
+// };
 
-export default BlinkingCursor;
+// export default BlinkingCursor;
