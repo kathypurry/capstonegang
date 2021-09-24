8772438fbb2e34fa83dcfddc54d6609bc456a73d
Candice <Candice.Agard@gmail.com>
2021-09-23 23:59:08 -0400

    input field more accessible

--- front-end/src/Pages/testPage.js
+++ front-end/src/Pages/testPage.js
@@ -9,17 +9,9 @@ const TestPage = () => {
         <div>
             {/* <StatGraph/>
             <Timer expiryTimestamp={30} /> */}
-<<<<<<< HEAD
-<<<<<<< HEAD
-            {/* <MainMenu/> */}
-=======
+
             <MainMenu />
->>>>>>> 15618454ad6318401c1484154ce7d7a15ec6e1af
-=======
 
-            {/* <MainMenu/> */}
-
->>>>>>> 34460d53b31588a82720e365ed3b46211a11e947
         </div>
     )
 };
