2179c7808e3f2b302ee5a33773e397a559004efc
Jon Narine <jonnicwolf@gmail.com>
2021-09-30 23:09:17 -0400

    randomise snippets during page refresh

--- front-end/src/Pages/GamePage.js
+++ front-end/src/Pages/GamePage.js
@@ -6,45 +6,47 @@ import axios from 'axios';
 import { apiURL } from '../util/apiURL';
 
 function GamePage() {
-    const API = apiURL();
-    // const [ allSnippets, setAllSnippets ] = useState([]);
-    const [ difficulty, setDifficulty ] = useState('');
-    const [ currentSnippet, setCurrentSnippet ] = useState({});
+   const API = apiURL();
+   // const [ allSnippets, setAllSnippets ] = useState([]);
+   const [ difficulty, setDifficulty ] = useState('');
+  const [currentSnippet, setCurrentSnippet] = useState({});
 
-    useEffect(() => {
-        axios
-          .get(`${API}/code/${difficulty}`)
-          .then(
-            (res) => {
-            //   setAllSnippets(res.data)
-              setCurrentSnippet(res.data[0])
-            },
-            (error) => console.log('get', error)
-          )
-          .then(res=>console.log(res))
-            .catch((c) => console.warn('catch', c));
+  const dice = (nat, min) => {
+    return Math.floor(Math.random() * nat + min);
+  };
+
+
+  useEffect(() => {
+      axios
+        .get(`${API}/code/${difficulty}`)
+        .then(
+          (res) => {
+            setCurrentSnippet(res.data[dice(res.data.length, 0)])
+          },
+          (error) => console.log('get', error))
+        .then(res=>console.log(res))
+        .catch((c) => console.warn('catch', c));
       console.log(currentSnippet, 'test curSnippet');
-        // console.log(allSnippets, 'test allSnippets');
-      }, [difficulty]);
+  }, [difficulty]);
 
-    const handleDifficultyChange = (e) => {
-        setDifficulty(e.target.value); 
-    };
+  const handleDifficultyChange = (e) => {
+      setDifficulty(e.target.value); 
+  };
 
-    return (
-        <div>
-            {difficulty === ''
-                ? null
-                : <PlayerInput snippet={currentSnippet.snippet}/> 
-            }
-            
-            <SelectDifficulty 
-                handleDifficultyChange={handleDifficultyChange} 
-                difficulty={difficulty} 
-            />
-            
-        </div>
-    );
+  return (
+      <div>
+          {difficulty === ''
+              ? null
+              : <PlayerInput snippet={currentSnippet.snippet}/> 
+          }
+          
+          <SelectDifficulty 
+              handleDifficultyChange={handleDifficultyChange} 
+              difficulty={difficulty} 
+          />
+          
+      </div>
+  );
 };
 
 export default GamePage;
