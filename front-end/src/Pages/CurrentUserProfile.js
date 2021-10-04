29ee7a4857af8b71b355d6a46cf385ef46465f20
Candice Agard <candy@Candices-MacBook-Pro.local>
2021-10-04 14:16:03 -0400

    user ref error resolved

--- front-end/src/Pages/CurrentUserProfile.js
+++ front-end/src/Pages/CurrentUserProfile.js
@@ -5,6 +5,21 @@ import { collection, doc, setDoc } from "firebase/firestore";
 import { db } from './Services/firebase';
 const CurrentUserProfile = () => {
 
+    async function readAllUsers() {
+        try {
+      
+          const usersRef = collection(db,'users');
+         
+          console.log(`User Reference`, usersRef);
+      
+        }
+        catch (err) {
+          console.error(err);
+        }
+      }
+      
+      readAllUsers();
+
     return (
         <div>
 
