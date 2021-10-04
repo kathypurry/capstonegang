223955f2273f0fb2b5fa83e15a02b7a4995ab4a0
Candice Agard <candy@Candices-MacBook-Pro.local>
2021-10-04 14:14:37 -0400

    current user profile for uid configuration

---
+++ front-end/src/Pages/CurrentUserProfile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// firebase firestore
+import { collection, doc, setDoc } from "firebase/firestore"; 
+import { db } from './Services/firebase';
+const CurrentUserProfile = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CurrentUserProfile;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 // firebase
-import { collection, doc, setDoc } from "firebase/firestore"; 
-import { db } from './Services/firebase';
+// import { collection, doc, setDoc } from "firebase/firestore"; 
+// import { db } from './Services/firebase';
 
 
 ReactDOM.render(
@@ -18,20 +18,20 @@ ReactDOM.render(
 
 console.log(db);
 
-async function readAllUsers() {
-  try {
+// async function readAllUsers() {
+//   try {
 
-    const usersRef = collection(db,'users');
-  
-    console.log(`User Reference`, usersRef);
+//     const usersRef = collection(db,'users');
+   
+//     console.log(`User Reference`, usersRef);
 
-  }
-  catch (err) {
-    console.error(err);
-  }
-}
+//   }
+//   catch (err) {
+//     console.error(err);
+//   }
+// }
 
-readAllUsers();
+// readAllUsers();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
