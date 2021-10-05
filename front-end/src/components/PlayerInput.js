0b1a2af25e686ce2cbdfef430b85c6e30b36acc5
Candice Agard <candy@macbook-pro.myfiosgateway.com>
2021-10-05 13:21:17 -0400

    resolved type error issue around setTypeWrong function

--- front-end/src/components/PlayerInput.js
+++ front-end/src/components/PlayerInput.js
@@ -92,7 +92,7 @@ const PlayerInput = ({ snippet }) => {
         }
         insertTyping(" ");
         setTypingInput("");
-        setTypeWrong(false);
+        // setTypeWrong(false);
     };
   
     useEffect(() => {
