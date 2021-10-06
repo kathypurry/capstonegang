8af39aea0a6c68f118be4549ac1cee04f46cf1c7
Kathy Gee <mzchick@gmail.com>
2021-10-05 23:18:10 -0400

    centered SelectDifficulty.js

--- front-end/src/Pages/About.js
+++ front-end/src/Pages/About.js
@@ -223,16 +223,7 @@ const AboutComponent = () => {
         <Content>
           <h2>Kathy Gee</h2>
           <p>
-            Lorem ipsum "Lorem ipsum dolor sit amet, <br />
-            consectetur adipiscing elit, <br />
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-            <br />
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. <br />
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. <br />
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum."
+              I'm too cool for Lorem Ipsum
           </p>
         </Content>
       </Section>
