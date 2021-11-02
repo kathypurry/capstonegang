6ed4e3899d66f449198efcaf84f232f27b49f695
Kathy Gee <mzchick@gmail.com>
2021-11-02 15:24:43 -0400

    update heroku link

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -1,5 +1,7 @@
 export const apiURL = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:4000"
-    : "https://mysterious-spire-49483.herokuapp.com";
+    : "https://rocky-shelf-02458.herokuapp.com";
 };
+
+//https://mysterious-spire-49483.herokuapp.com
