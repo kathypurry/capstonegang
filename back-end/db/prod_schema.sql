6350e645b273fb299c06cba5034e1be339454a63
Grace Bong <gracebong.park@gmail.com>
2021-11-02 23:49:11 -0400

    syntax error fixed on prod schema

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -1,14 +1,11 @@
--- DROP DATABASE IF EXISTS dbdovavbhl8c03;
--- CREATE DATABASE dbdovavbhl8c03;
-
 \c dbdovavbhl8c03;
 
 DROP TABLE IF EXISTS username;
 
 CREATE TABLE username (
     id SERIAL PRIMARY KEY, 
-    username TEXT
-    email TEXT
+    username TEXT,
+    email TEXT,
     password TEXT
 );
 
@@ -16,19 +13,17 @@ DROP TABLE IF EXISTS code_snippets ;
 
 CREATE TABLE code_snippets (
     id SERIAL PRIMARY KEY, 
-    difficulty NUMBER
+    difficulty NUMBER,
     snippet TEXT
- 
 );
 
 DROP TABLE IF EXISTS game_stats;
 
 CREATE TABLE game_stats (
     id SERIAL PRIMARY KEY, 
-    percentage NUMBER
-    wpm NUMBER
-    correct NUMBER
-    incorrect NUMBER
+    percentage NUMBER,
+    wpm NUMBER,
+    correct NUMBER,
+    incorrect NUMBER,
     missed NUMBER
-   
 );
